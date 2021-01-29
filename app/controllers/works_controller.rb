276029605f748947d650781c3646b626e6ec82f1
Yuna Kusaka <loxoo_1@yahoo.co.jp>
2021-01-29 19:14:23 +0900

    add show and edit page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -6,4 +6,31 @@ class WorksController < ApplicationController
   def show
     @work = Work.find(params[:id])
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+   @work = Work.new(work_params)
+   if @work.save
+     redirect_to works_path
+   else
+     render 'new'
+   end
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    if @work.update(work_params)
+      redirect_to work_path
+    else
+      render 'edit'
+    end
+  end
+
 end
